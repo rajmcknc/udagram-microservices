bcd87c945f29686729505f8536147308948c17d5
RajT <rajmcknc@gmail.com>
2020-11-18 01:14:56 -0500

    First stable code
    This code works with docker

---
+++ udagram-feed-api/set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRESS_USERNAME=
+export process.env.POSTGRESS_PASSWORD=
+export process.env.POSTGRESS_DATABASE=
+export process.env.POSTGRESS_HOST= 
+export process.env.AWS_REGION=
+export process.env.AWS_PROFILE=default
+export process.env.AWS_MEDIA_BUCKET=
+export process.env.URL=
+export process.env.UI_URL=
+export process.env.JWT_SECRET=
---
+++ udagram-feed-api/src/config/config.ts
@@ -0,0 +1,15 @@
+export const config = {
+    'username': process.env.POSTGRESS_USERNAME,
+    'password': process.env.POSTGRESS_PASSWORD,
+    'database': process.env.POSTGRESS_DATABASE,
+    'host': process.env.POSTGRESS_HOST,
+    'dialect': 'postgres',
+    'aws_region': process.env.AWS_REGION,
+    'aws_profile': process.env.AWS_PROFILE,
+    'aws_media_bucket': process.env.AWS_MEDIA_BUCKET,
+    'url': process.env.URL,
+    'ui_url': process.env.UI_URL,
+    'jwt': {
+        'secret': process.env.JWT_SECRET,
+    },
+};

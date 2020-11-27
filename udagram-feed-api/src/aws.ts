e58f16e59d5a22a2d6313ae6c51f35e80ac8e951
RajT <rajmcknc@gmail.com>
2020-11-27 10:38:17 -0500

    added aws access key and access secret

--- udagram-feed-api/src/aws.ts
+++ udagram-feed-api/src/aws.ts
@@ -7,6 +7,8 @@ const credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
 AWS.config.credentials = credentials;
 
 export const s3 = new AWS.S3({
+  accessKeyId: config.aws_access_key_id,
+  secretAccessKey: config.aws_secret_access_key,
   signatureVersion: 'v4',
   region: config.aws_region,
   params: {Bucket: config.aws_media_bucket},

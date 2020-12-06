d16a5026e8e0465ad1d664ea371a48c70db8ab98
RajT <rajmcknc@gmail.com>
2020-12-05 23:29:36 -0500

    Work on travis

--- udagram-feed-api/src/aws.ts
+++ udagram-feed-api/src/aws.ts
@@ -1,7 +1,6 @@
 import AWS = require('aws-sdk');
 import {config} from './config/config';
 
-
 // Configure AWS
 const credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
 AWS.config.credentials = credentials;
